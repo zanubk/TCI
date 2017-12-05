3f62bc4c3559e165f584488ce47e63fb544809d9
Khan,Zanub Khan Z <z.khan@student.fontys.nl>
2017-12-05 15:56:59 +0100

    second

--- src/test/java/CTest.java
+++ src/test/java/CTest.java
@@ -2,11 +2,15 @@
 
 import  org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 public class CTest {
 
     @Test
     public void Try()
-    {assertTrue(false);}
+    {
+        int n = 9;
+        assertEquals(94,n);
+    }
 
 }
